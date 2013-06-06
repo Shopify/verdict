346288c810259f2d4133c0581f0710c9ad3a54c0
Willem van Bergen <willem@vanbergen.org>
2013-06-06 15:42:36 -0400

    Fix Railtie.

--- lib/experiments/railtie.rb
+++ lib/experiments/railtie.rb
@@ -1,2 +1,6 @@
-Experiments.logger = Rails.logger
-Experiments.directory = Rails.root.join('app', 'experiments')
+class Experiments::Railtie < Rails::Railtie
+  initializer "experiments.configure_rails_initialization" do 
+    Experiments.logger = Rails.logger
+    Experiments.directory = Rails.root.join('app', 'experiments')
+  end
+end
