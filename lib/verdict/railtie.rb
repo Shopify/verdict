d9a9e06d76cb4b10acc5aef6f8aac32d6e41312f
José Albornoz <jojo@eljojo.net>
2017-04-19 14:49:58 -0400

    Don't overwrite Verdict.directory, and support multiple paths

--- lib/verdict/railtie.rb
+++ lib/verdict/railtie.rb
@@ -1,9 +1,10 @@
 class Verdict::Railtie < Rails::Railtie
   initializer "experiments.configure_rails_initialization" do |app|
     Verdict.default_logger = Rails.logger
-    Verdict.directory = Rails.root.join('app', 'experiments')
 
-    app.config.eager_load_paths -= [Verdict.directory.to_s]
+    Verdict.directory ||= Rails.root.join('app', 'experiments')
+    app.config.eager_load_paths -= Dir[Verdict.directory.to_s]
+
     # Re-freeze eager load paths to ensure they blow up if modified at runtime, as Rails does
     app.config.eager_load_paths.freeze
   end
