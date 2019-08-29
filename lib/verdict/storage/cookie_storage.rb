ff3db8815df736ec7730e54dabef090352791f0b
Brendan Abbott <brendan.abbott@shopify.com>
2019-08-29 15:00:26 -0400

    Add CookieStorage backend

---
+++ lib/verdict/storage/cookie_storage.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+module Verdict
+  module Storage
+    # CookieStorage, unlike other Verdict storage classes, is distributed and stored on the client.
+    # Because cookies are opaque to users, we obsfucate information about the test such as the
+    # human readable names for the experiment or assignment group. This means this class assumes
+    # that `name` will be an obsfucated value, or one that we comfortable being "public".
+    class CookieStorage < BaseStorage
+      DEFAULT_COOKIE_LIFESPAN_SECONDS = 15778476 # 6 months
+
+      attr_accessor :cookies
+      attr_reader :cookie_lifespan
+
+      def initialize(options = {})
+        @cookies = options[:cookies] || {}
+        @cookie_lifespan = options[:cookie_lifespan] || DEFAULT_COOKIE_LIFESPAN_SECONDS
+      end
+
+      def store_assignment(assignment)
+        hash = { group: assignment.group.name, created_at: assignment.created_at.strftime('%FT%TZ') }
+        set("verdict_#{assignment.experiment.name}", nil, JSON.dump(hash))
+      end
+
+      def retrieve_assignment(experiment, subject)
+        if (value = get("verdict_#{experiment.name}", nil))
+          hash = parse_cookie_value(value)
+          group = experiment.groups.values.find { |g| g.name == hash['group'] }
+
+          if group.nil?
+            experiment.remove_subject_assignment(subject)
+            return nil
+          end
+
+          experiment.subject_assignment(subject, group, Time.xmlschema(hash['created_at']))
+        end
+      end
+
+      def remove_assignment(experiment, _subject)
+        remove("verdict_#{experiment.name}", nil)
+      end
+
+      def retrieve_start_timestamp(_experiment)
+        nil
+      end
+
+      def store_start_timestamp(_experiment, _timestamp)
+        true
+      end
+
+      protected
+
+      def get(scope, _key)
+        cookies[scope]
+      end
+
+      def set(scope, _key, value)
+        cookies[scope] = {
+          value: value,
+          expires: Time.now.utc.advance(seconds: cookie_lifespan),
+        }
+      end
+
+      def remove(scope, _key)
+        cookies.delete(scope)
+      end
+
+      private
+
+      def parse_cookie_value(value)
+        value = value[:value] if value.is_a?(Hash)
+
+        JSON.parse(value)
+      rescue
+        {}
+      end
+    end
+  end
+end
---
+++ test/storage/cookie_storage_test.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class CookieStorageTest < Minitest::Test
+  def setup
+    @storage = storage = Verdict::Storage::CookieStorage.new
+    @experiment = Verdict::Experiment.new(:cookie_storage_test) do
+      name 1234
+      groups do
+        group :all, 100 do name 5678
+        end
+      end
+      storage storage, store_unqualified: true
+    end
+    @subject = stub(id: 'bob')
+    @assignment = Verdict::Assignment.new(@experiment, @subject, @experiment.group(:all), nil)
+  end
+
+  def test_cookie_lifespan_has_a_default
+    refute_nil Verdict::Storage::CookieStorage.new.cookie_lifespan
+  end
+
+  def test_cookie_lifespan_can_be_configured
+    storage = Verdict::Storage::CookieStorage.new(cookie_lifespan: 60)
+
+    assert_equal 60, storage.cookie_lifespan
+  end
+
+  def test_store_assignment_returns_true_when_an_assignment_is_stored
+    assert @storage.store_assignment(@assignment)
+  end
+
+  def test_retrieve_assignment_returns_an_assignment
+    @storage.store_assignment(@assignment)
+    assignment = @storage.retrieve_assignment(@experiment, @subject)
+
+    assert assignment.returning?
+    assert_equal :all, assignment.handle.to_sym
+    assert_equal @experiment, assignment.experiment
+    assert_equal @subject, assignment.subject
+  end
+
+  def test_retrieve_assignment_returns_nil_when_an_assignment_does_not_exist
+    assert_nil @storage.retrieve_assignment(@experiment, @subject)
+  end
+
+  def test_retrieve_assignment_returns_nil_when_the_assignment_group_is_invalid
+    invalid_group = Verdict::Group.new(@experiment, :invalid)
+    invalid_group.name('invalid')
+    assignment = Verdict::Assignment.new(@experiment, @subject, invalid_group, nil)
+
+    @storage.store_assignment(assignment)
+
+    assert_nil @storage.retrieve_assignment(@experiment, @subject)
+  end
+
+  def test_remove_assignment_returns_true_when_removing_an_assignment
+    @storage.store_assignment(@assignment)
+
+    assert @storage.remove_assignment(@experiment, nil)
+    assert_nil @storage.retrieve_assignment(@experiment, @subject)
+  end
+
+  def test_retrieve_start_timestamp_always_returns_nil
+    assert_nil @storage.retrieve_start_timestamp(nil)
+  end
+
+  def test_store_start_timestamp_always_returns_true
+    assert @storage.store_start_timestamp(nil, nil)
+  end
+end
